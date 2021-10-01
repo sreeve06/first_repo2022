71205c9026c1274c608dd323e29e68f6a504aaa2
sreeve06 <91707016+sreeve06@users.noreply.github.com>
2021-10-01 09:42:36 -0700

    Create project_0.py

---
+++ project_0.py
@@ -0,0 +1,12 @@
+greeting = "Hello Github"
+problem = "I have no idea what I'm doing."
+
+solution = input("Should I practice to get better? y or no: ")
+  if solution == "y":
+    print("Ok, I'll practice. Thanks for the advice.")
+  else:
+    print("Guess I'm out of luck then. It's going to be a long term...")
+
+print(greeting)
+print(problem)
+print(solution)
