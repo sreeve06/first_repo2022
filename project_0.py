0bfa8155ba5cdf924a59e24266069bdb18f81872
sreeve06 <91707016+sreeve06@users.noreply.github.com>
2021-10-01 09:46:58 -0700

    Update project_0.py

--- project_0.py
+++ project_0.py
@@ -1,12 +1,13 @@
 greeting = "Hello Github"
 problem = "I have no idea what I'm doing."
 
-solution = input("Should I practice to get better? y or no: ")
+print(greeting)
+print(problem)
+
+solution = input("Should I practice to get better? y or n: ")
   if solution == "y":
     print("Ok, I'll practice. Thanks for the advice.")
   else:
     print("Guess I'm out of luck then. It's going to be a long term...")
 
-print(greeting)
-print(problem)
-print(solution)
+
